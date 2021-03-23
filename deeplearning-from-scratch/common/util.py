84e7188c09307e8d58f112ef9f21e1556559bac2
sunrui <1527717978@qq.com>
2021-03-23 09:49:59 +0800

    Update util.py
    添加im2col注释

--- deeplearning-from-scratch/common/util.py
+++ deeplearning-from-scratch/common/util.py
@@ -64,7 +64,7 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
             x_max = x + stride*out_w
             col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
 
-    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1) #　列为C*filter_h*filter_w
     return col
 
 
